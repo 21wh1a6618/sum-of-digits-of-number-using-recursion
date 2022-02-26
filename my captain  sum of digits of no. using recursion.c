9cf20bbda50962dbeb049af90e04d4944e16b8a5
21wh1a6618 <96616374+21wh1a6618@users.noreply.github.com>
2022-02-26 21:47:39 +0530

    Add files via upload

---
+++ my captain  sum of digits of no. using recursion.c
@@ -0,0 +1,27 @@
+//c program to find sum of digits of number using recursion//
+#include<stdio.h>
+int sum(int a);
+int main()
+{
+  int num,result;
+  
+   printf("enter the numbers:");
+   scanf("%d",&num);
+   result=sum(num);
+   printf("sum of digits in %d is %d\n",num,result);
+   return 0;
+}
+ int sum(int num)  
+{
+   if(num!=0)
+   {
+     return(num%10+sum(num/10));
+   }
+   else
+   {
+    return 0;
+   }
+   
+   
+}
+   
